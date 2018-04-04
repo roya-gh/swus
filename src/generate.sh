eb890273fea3c5f44c98c14558eac92e0c5778d7
Soroush Rabiei <soroush@ametisco.ir>
2018-04-04 14:10:27 +0430

    Add typed web service definition
    Signed-off-by: Soroush Rabiei <soroush@ametisco.ir>

--- src/generate.sh
+++ src/generate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wsdl2h -c++11 -p -oSWUS.h ../docs/service.wsdl
-soapcpp2 -2 -S -L -c++11 -b -e -i -w -x ./SWUS.h
+wsdl2h -c++11 -g -a -w -y -oSWUS.h ../docs/service.wsdl
+soapcpp2 -2 -S -a -A -t -c++11 -b -i -w -x ./SWUS.h
